2c0d40a7d9dd475836d2f40960623c980b6757f0
successfulcode <sergej.mickevic@gmail.com>
2021-06-08 22:16:30 +0300

    Installed awesome icons, updated login template

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import i18n from './i18n';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faLock,
+  faEnvelope,
+  faExclamationTriangle,
+  faCheck
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faLock, faEnvelope, faExclamationTriangle, faCheck);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,80 @@
 <template>
-  <div>{{ $t('common.login') }}</div>
+  <div class="columns is-flex is-justify-content-center">
+    <form class="column is-3 mt-5" @submit.prevent="onSubmit">
+      <div class="has-text-centered is-size-3">{{ $t('common.login') }}</div>
+      <div class="field mt-5">
+        <div class="control has-icons-left has-icons-right">
+          <input
+            class="input is-danger"
+            type="email"
+            :placeholder="$t('common.email')"
+            v-model="email"
+          />
+          <span class="icon is-small is-left">
+            <font-awesome-icon :icon="['fa', 'envelope']" class="mt-3 ml-2" />
+          </span>
+          <span class="icon is-small is-right">
+            <font-awesome-icon
+              v-if="isInvalid"
+              :icon="['fa', 'exclamation-triangle']"
+              class="mt-3 ml-2"
+            />
+            <font-awesome-icon v-else :icon="['fa', 'check']" class="mt-3 ml-2" />
+          </span>
+        </div>
+        <p class="help is-danger">This email is invalid</p>
+      </div>
+      <div class="field">
+        <div class="control has-icons-left has-icons-right">
+          <input
+            class="input is-danger"
+            type="text"
+            :placeholder="$t('common.password')"
+            v-model="password"
+          />
+          <span class="icon is-small is-left">
+            <font-awesome-icon :icon="['fa', 'lock']" class="mt-3 ml-2" />
+          </span>
+          <span class="icon is-small is-right">
+            <font-awesome-icon
+              v-if="isInvalid"
+              :icon="['fa', 'exclamation-triangle']"
+              class="mt-3 ml-2"
+            />
+            <font-awesome-icon v-else :icon="['fa', 'check']" class="mt-3 ml-2" />
+          </span>
+        </div>
+        <p class="help is-danger">This email is invalid</p>
+      </div>
+      <div class="is-flex is-justify-content-center">
+        <button class="button is-info" type="submit" :disabled="isInvalid">Patvirtinti</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'Login',
+  data() {
+    return {
+      isInvalid: true,
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    onSubmit() {
+      const user = {
+        email: this.email,
+        password: this.password
+      };
+      console.log('onSubmit', user);
+      this.email = '';
+      this.password = '';
+    }
+  }
+};
 </script>
 
 <style></style>
