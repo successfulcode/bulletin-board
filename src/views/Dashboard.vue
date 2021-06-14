2de88d2dbbb714c7116c302eb05e2dd5524a06a8
successfulcode <sergej.mickevic@gmail.com>
2021-06-14 22:55:12 +0300

    Made Create ad component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,24 +1,26 @@
 <template>
   <div>
-    <div>Dashboard</div>
-    <button @click="testData">TEST</button>
+    <create-ad></create-ad>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+// import axios from 'axios';
+import CreateAd from '@/components/CreateAd.vue';
 
 export default {
-  methods: {
-    async testData() {
-      const test = 'test';
-      try {
-        const response = await axios.post(process.env.VUE_APP_DATA_URL, { test });
-        console.log('respones', response.data);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  }
+  name: 'Dashboard',
+  components: { CreateAd }
+  // methods: {
+  //   async testData() {
+  //     const test = 'test';
+  //     try {
+  //       const response = await axios.post(process.env.VUE_APP_DATA_URL, { test });
+  //       console.log('respones', response.data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   }
+  // }
 };
 </script>
