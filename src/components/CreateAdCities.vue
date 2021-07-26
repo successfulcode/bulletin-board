d9e80a555c5d804afa0f0c9b603def71cec75c2c
successfulcode <sergej.mickevic@gmail.com>
2021-07-26 18:25:50 +0300

    Made add city function

---
+++ src/components/CreateAdCities.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="select">
+      <select class="input" @change="$emit('setCity', $event.target.value)">
+        <option :value="$t('cities.allCities')">{{ $t('cities.allCities') }}</option>
+        <optgroup :label="$t('cities.bigCities')">
+          <option :value="cityItem.city" v-for="cityItem in bigCities" :key="cityItem.city">
+            {{ cityItem.city }}
+          </option>
+        </optgroup>
+        <optgroup :label="$t('cities.other')">
+          <option :value="cityItem.city" v-for="cityItem in other" :key="cityItem.city">
+            {{ cityItem.city }}
+          </option>
+        </optgroup>
+        <optgroup :label="$t('cities.otherCities')">
+          <option :value="cityItem.city" v-for="cityItem in otherCities" :key="cityItem.city">
+            {{ cityItem.city }}
+          </option>
+        </optgroup>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+import { bigCities, otherCities, other } from '../assets/cities';
+export default {
+  data() {
+    return {
+      bigCities: bigCities,
+      otherCities: otherCities,
+      other: other
+    };
+  }
+};
+</script>
